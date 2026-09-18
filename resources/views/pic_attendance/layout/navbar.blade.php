f75a61011fbba60a1dc190370cb07877383c0afd
msyafiqdev <msyafiq.dev@gmail.com>
2026-09-18 15:02:56 +0700

    pic navbar

--- resources/views/pic_attendance/layout/navbar.blade.php
+++ resources/views/pic_attendance/layout/navbar.blade.php
@@ -1,6 +1,7 @@
 <div class="card h-auto px-md-2 pt-md-2">
     <div class="card-header py-0 d-flex flex-wrap justify-content-between align-items-center mx-sm-4 px-0">
         <ul class="nav nav-underline gap-3 nav-scroll nav-scroll-auto-xl px-3 px-sm-0" role="tablist">
+            @if(!request()->routeIs('pic-attendance.task*'))
             <li class="nav-item" role="presentation">
                 <a href="{{ route('pic-attendance.attendance') }}" class="nav-link py-3 px-1 border-3 {{ request()->routeIs('pic-attendance.attendance*') ? 'active' : '' }}">Attendance</a>
             </li>
@@ -10,9 +11,11 @@
             <li class="nav-item" role="presentation">
                 <a href="{{ route('pic-attendance.overtime') }}" class="nav-link py-3 px-1 border-3 {{ request()->routeIs('pic-attendance.overtime*') ? 'active' : '' }}">Overtime</a>
             </li>
-            <li class="nav-item" role="presentation">
-                <a href="{{ route('pic-attendance.task') }}" class="nav-link py-3 px-1 border-3 {{ request()->routeIs('pic-attendance.task*') ? 'active' : '' }}">Task</a>
-            </li>
+            @else
+                <li class="nav-item" role="presentation">
+                    <a href="{{ route('pic-attendance.task') }}" class="nav-link py-3 px-1 border-3 active">Team Tasks</a>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
